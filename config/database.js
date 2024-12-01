76fdb82f4a883cf5f576aacba6cb1f9ad7d035f6
istimats <152053117+istimats@users.noreply.github.com>
2024-12-02 01:54:39 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('auth_db', 'root', '',{
-    host: 'localhost',
+const db = new Sequelize('bhdhbg68l41r537hvbkg', 'um32ou1xblawcsit', 'rM77Xalb7t0M80TFfvSK',{
+    host: 'bhdhbg68l41r537hvbkg-mysql.services.clever-cloud.com',
     dialect: 'mysql'
 })
 
